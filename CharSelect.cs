b20ccd3635ef20170628237a339c6d43f75e30e5
Adriana Sawyer <asawyer@aggies.ncat.edu>
2017-10-29 20:43:55 -0400

    Add files via upload

---
+++ CharSelect.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+// Loads Char sprites from Resources folder
+// and allows them to be chosen by the selection button.
+//Adriana Sawyer
+
+public class CharSelect : MonoBehaviour
+{
+    private string spriteNames = "Hero";
+    private Rect buttonPos;
+    private int spriteVersion = 0;
+    private SpriteRenderer spriteR;
+    private Sprite[] sprites;
+
+    void Start()
+    {
+        buttonPos = new Rect(10.0f, 10.0f, 150.0f, 50.0f);
+        spriteR = gameObject.GetComponent<SpriteRenderer>();
+        sprites = Resources.LoadAll<Sprite>(spriteNames);
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(buttonPos, "Choose next sprite"))
+        {
+            spriteVersion += 1;
+            if (spriteVersion > 3)
+                spriteVersion = 0;
+            spriteR.sprite = sprites[spriteVersion];
+        }
+    }
+}
+}
---
+++ WordsAppear.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+
+public class WordsAppear : MonoBehaviour {
+    //Adriana Sawyer
+    public GameObject secr;
+    Sprite[] tiles = Resources.LoadAll<Sprite>("Tile");
+    private var bool : boolean;
+    // Use this for initialization
+    void Start(){
+    }
+    // Update is called once per frame
+    void Update(){
+    }
+
+    void OnTriggerStay2D(Collider2D collider) {
+        //Sprite atiles = new Sprite;
+        //if (collider.gameObject.tag == "Enemy"){
+        foreach(Sprite tile in tiles){
+            gameObject.layer = 2;
+            bool = true;
+
+        }
+        bool = false;
+        //}
+        //use this if you want to switch it back after
+        //else{  
+        //}
+    }
+    function OnGUI(){
+        if (bool == true){
+            GUI.Button(Rect(100, 100, 500, 40), "Spell the word to defeat him!      Click to close.");
+             //Debug.Log("Great Work! Onto the rest of the town!");
+             bool = false;
+        //end if
+        }
+    //end GUI funct
+    }
+
+
+
+    //end func
+}
+    //end class
+//}
